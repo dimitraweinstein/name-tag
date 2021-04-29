be2ab86260a1a51fd93fe1743ab084c83680cd21
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-04-29 14:00:33 -0700

    Submit button is now working, produces name when entered

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const userInput = document.getElementById('user-input');
 const myButton = document.getElementById('button');
 const wordBox = document.getElementById('word-box');
 
-
-
 myButton.addEventListener('click', () => {
-    wordBox.textContent = userInput;
+    const name = userInput.value;
+    wordBox.textContent = name;
 })
