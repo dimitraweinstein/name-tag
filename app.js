e028cbb33566426560177bcca741d1c07e7c3f04
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-04-30 16:02:22 -0700

    updated js to make color buttons work correctly

--- app.js
+++ app.js
@@ -16,13 +16,27 @@ pronounButton.addEventListener('click', () => {
     pronounWordBox.textContent = stuff;
 })
 
-const colorButton1 = document.getElementById('main-color');
-const colorButton2 = document.getElementById('main-color');
-const colorButton3 = document.getElementById('main-color');
-
-const mainColor = document.getElementById('main-color');
+const colorButton1 = document.getElementById('color-button1');
+const colorButton2 = document.getElementById('color-button2');
+const colorButton3 = document.getElementById('color-button3');
+const backgroundColor = document.getElementById('main-color');
+const bottomColor = document.getElementById('main-color2');
 
 colorButton1.addEventListener('click', () => {
-    const newColor1 = rgb('27, 23, 37')
-    backgroundColor.style.background = newColor1;    
+    const newColor1 = 'rgb(27, 23, 37)';
+    backgroundColor.style.background = newColor1;
+    bottomColor.style.background = newColor1;    
+})
+
+colorButton2.addEventListener('click', () => {
+    const newColor2 = 'rgb(164, 153, 179)';
+    backgroundColor.style.background = newColor2;
+    bottomColor.style.background = newColor2;
+})
+
+colorButton3.addEventListener('click', () => {
+    const newColor3 = 'rgb(34, 108, 224)';
+    backgroundColor.style.background = newColor3;
+    bottomColor.style.background = newColor3;    
+    
 })
